f930748e3d1dff1d846c045afa2463625e9270c4
luischicllarosas <luischicllarosas@gmail.com>
2020-12-21 02:58:07 -0500

    firts commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
 import Vue from 'vue'
+// BootstrapVue
+import './plugins/bootstrapVue'
+
+// Vue Font-Awesome
+import './plugins/fontAwesome'
+
+// Custom directives
+import './directives'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// CSS global
+import './assets/css/main.styl'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  methods: {
+    // Nuestra función
+    init () {
+      store.dispatch('oauth/getToken')
+    }
+  },
+  // Hook created
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
